95fe4234022e03581af05ba4e2e32eceab3de436
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-06-15 00:07:45 -0400

    Make basic calculator operation functions

---
+++ script.js
@@ -0,0 +1,23 @@
+let numberA = 0;
+let numberB = 0;
+let operator = "";
+
+function calcSum(a,b) {
+    let newSum = a + b;
+    return newSum
+}
+
+function calcSubt(a,b) {
+    let newSubt = a - b;
+    return newSubt
+}
+
+function calcMult(a,b) {
+    let newMult = a * b;
+    return newMult
+}
+
+function calcDiv(a,b) {
+    let newDiv = a / b;
+    return newDiv
+}
