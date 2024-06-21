8cb51d062c09de067afb88c1a08f93fe8169ffc4
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-06-20 22:30:52 -0400

    Create function for result calculation

--- script.js
+++ script.js
@@ -1,61 +1,94 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    operate();
+});
 
 function calcSum(a,b) {
     let newSum = a + b;
-    return newSum
+    return newSum;
 }
 
 function calcSubt(a,b) {
     let newSubt = a - b;
-    return newSubt
+    return newSubt;
 }
 
 function calcMult(a,b) {
     let newMult = a * b;
-    return newMult
+    return newMult;
 }
 
 function calcDiv(a,b) {
     let newDiv = a / b;
-    return newDiv
+    return newDiv;
 }
 
 function operate() {
-    let operator = null;
-    let numberA = null;
-    let numberB = null;
+    let operator = "";
+    let numberA = 0;
+    let numberB = 0;
     let displayText= "";
+    let answer=0;
     const screenText = document.querySelector("#update");
     const buttonOperator = document.querySelectorAll(".operator");
     const buttonNumber = document.querySelectorAll(".number");
+    const calcResult = document.querySelector(".result");
     
         buttonOperator.forEach(button => {
             button.addEventListener("click", () => {
-                operator = button.getAttribute("data-value");
-                screenText.textContent = displayText + operator;
+                operator = button.textContent;
+                displayText += operator;
+                screenText.textContent = displayText;
             });
         });
 
         buttonNumber.forEach(button => {
             button.addEventListener("click", () => {
-                if (!operator){
-                numberA = parseFloat(button.textContent);
+                if (operator === ""){
+                numberA = Number(button.textContent);
+                
                 
                 } else {
-                numberB = parseFloat(button.textContent);
+                numberB = Number(button.textContent);
+                
                 
                 }
+                
                 displayText += button.textContent;
                 screenText.textContent = displayText;
+                
 
              })
         });
 
-    
-    
         
+        if (calcResult) {
+            calcResult.addEventListener("click", () => {
+                
+                    if (operator === "+") {
+                        console.log(numberA);
+                    console.log(numberB);
+                        answer = calcSum(numberA, numberB);
+                        
+                    } else if (operator === "-") {
+                        answer = calcSubt(numberA, numberB);
+                    } else if (operator === "X") {
+                        answer = calcMult(numberA, numberB);
+                    } else if (operator === "/") {
+                        answer = calcDiv(numberA, numberB);
+                    } else {
+                        console.log(" its 0 ")
+                    }
+                    screenText.textContent = answer;
+                    console.log(numberA);
+                    console.log(numberB);
+                    console.log(answer);
 
+                    
+                
+                });
+        
+            }
     
 }
 
-operate();
+
